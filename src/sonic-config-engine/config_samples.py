97c67a20a0d0a5dd4466c71e2786abac611a12b4
squid_ro <squid_ro@edge-core.com>
2019-05-15 04:40:46 -0400

    Remove unneeded default config.

--- src/sonic-config-engine/config_samples.py
+++ src/sonic-config-engine/config_samples.py
@@ -6,31 +6,31 @@ from natsort import natsorted
 def generate_t1_sample_config(data):
     data['DEVICE_METADATA']['localhost']['hostname'] = 'sonic'
     data['DEVICE_METADATA']['localhost']['type'] = 'LeafRouter'
-    data['DEVICE_METADATA']['localhost']['bgp_asn'] = '65100'
-    data['LOOPBACK_INTERFACE'] = {"Loopback0|10.1.0.1/32": {}}
-    data['BGP_NEIGHBOR'] = {}
-    data['DEVICE_NEIGHBOR'] = {}
-    data['INTERFACE'] = {}
-    port_count = 0
+#    data['DEVICE_METADATA']['localhost']['bgp_asn'] = '65100'
+#    data['LOOPBACK_INTERFACE'] = {"Loopback0|10.1.0.1/32": {}}
+#    data['BGP_NEIGHBOR'] = {}
+#    data['DEVICE_NEIGHBOR'] = {}
+#    data['INTERFACE'] = {}
+#    port_count = 0
     total_port_amount = len(data['PORT'])
     for port in natsorted(data['PORT'].keys()):
         data['PORT'][port]['admin_status'] = 'up'
         data['PORT'][port]['mtu'] = '9100'
-        local_addr = '10.0.{}.{}'.format(2 * port_count / 256, 2 * port_count % 256)
-        peer_addr = '10.0.{}.{}'.format(2 * port_count / 256, 2 * port_count % 256 + 1)
-        peer_name='ARISTA{0:02d}{1}'.format(1+port_count%(total_port_amount/2), 'T2' if port_count < (total_port_amount/2) else 'T0')
-        peer_asn = 65200 if port_count < total_port_amount/2 else 64001 + port_count - total_port_amount/2
-        data['INTERFACE']['{}|{}/31'.format(port, local_addr)] = {}
-        data['BGP_NEIGHBOR'][peer_addr] = {
-                'rrclient': 0,
-                'name': peer_name,
-                'local_addr': local_addr,
-                'nhopself': 0,
-                'holdtime': '180',
-                'asn': str(peer_asn),
-                'keepalive': '60'
-                }
-        port_count += 1
+#        local_addr = '10.0.{}.{}'.format(2 * port_count / 256, 2 * port_count % 256)
+#        peer_addr = '10.0.{}.{}'.format(2 * port_count / 256, 2 * port_count % 256 + 1)
+#        peer_name='ARISTA{0:02d}{1}'.format(1+port_count%(total_port_amount/2), 'T2' if port_count < (total_port_amount/2) else 'T0')
+#        peer_asn = 65200 if port_count < total_port_amount/2 else 64001 + port_count - total_port_amount/2
+#        data['INTERFACE']['{}|{}/31'.format(port, local_addr)] = {}
+#        data['BGP_NEIGHBOR'][peer_addr] = {
+#                'rrclient': 0,
+#                'name': peer_name,
+#                'local_addr': local_addr,
+#                'nhopself': 0,
+#                'holdtime': '180',
+#                'asn': str(peer_asn),
+#                'keepalive': '60'
+#                }
+#        port_count += 1
     return data;
 
 def generate_empty_config(data):
