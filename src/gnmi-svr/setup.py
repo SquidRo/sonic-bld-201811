b8e4cb8531dc6fe993ebcf33308b978aa23ff595
squid_ro <squid_ro@edge-core.com>
2019-09-11 02:39:02 -0400

    Enhance build_img2.sh and put git commit id info into gnmi_svr.whl.

--- src/gnmi-svr/setup.py
+++ src/gnmi-svr/setup.py
@@ -1,4 +1,14 @@
+import os, subprocess
 from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.realpath(__file__))
+wdir = os.path.join(here, 'my_gnmi_server')
+desc_str=''
+if os.path.exists(wdir):
+    git_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=wdir)
+
+    # use the git hash in the setup
+    desc_str = 'git hash [ %s ]' % git_hash.strip()
 
 dependencies = [
     'pyangbind',
@@ -8,6 +18,7 @@ setup(
     name='gnmi-svr',
     install_requires=dependencies,
     version='0.1',
+    description=desc_str,
     packages=find_packages(),
     license='Apache 2.0',
     author='',
